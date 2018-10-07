5c9a36cac7233764450f0a19aaf8fc6c44a46d1d
root <root@Discordia.localdomain>
2018-10-07 13:59:10 +0200

    Se deja más bonita la interfaz

--- src/servlet/ServletRegistro.java
+++ src/servlet/ServletRegistro.java
@@ -19,6 +19,23 @@ import model.Usuario;
 @WebServlet("/ServletRegistro")
 public class ServletRegistro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String TOP = "<!DOCTYPE html>\r\n" + "\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n" + "\r\n"
+			+ "	<meta charset=\"UTF-8\">\r\n" + "	<title>LOGIN FAIL</title>\r\n"
+			+ "	<link rel='icon' type='image/png' href=\"icon/favicon.png\" />\r\n"
+			+ "	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\r\n" + "\r\n"
+			+ "	<!-- Bootstrap CSS -->\r\n"
+			+ "	<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\">\r\n"
+			+ "	<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.3.1/css/all.css\">\r\n"
+			+ "	<link href=\"css/main.css\" rel=\"stylesheet\">\r\n" + "\r\n" + "</head>\r\n" + "\r\n" + "\r\n"
+			+ "<body>\r\n" + "\r\n" + "	<div class=\"container\" style=\"padding: 40px; text-align: center\">\r\n"
+			+ "		<div class=\"card card-container\" style=\"padding: 40px\"> <h1> ";
+	private static final String BOTTOM = "</div>\r\n" + " </h1>	</div>\r\n" + "\r\n"
+			+ "	<!-- jQuery first, then Popper.js, then Bootstrap JS -->\r\n"
+			+ "	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\r\n"
+			+ "	<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\"></script>\r\n"
+			+ "	<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\"></script>\r\n"
+			+ "\r\n" + "	<!-- Optional JavaScript -->\r\n" + "	<script src=\"js/main.js\"></script>\r\n" + "\r\n"
+			+ "</body>\r\n" + "\r\n" + "</html>";
 
 	public ServletRegistro() {
 		super();
@@ -26,11 +43,11 @@ public class ServletRegistro extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		/* No cacheamos la respuesta */
-		response.setHeader("Cache-Control","no-store");
-		response.setHeader("Pragma","no-cache");
-		response.setDateHeader ("Expires", 0);
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
 
 		// Instanciamos el Bean
 		Usuario u = new Usuario();
@@ -52,47 +69,33 @@ public class ServletRegistro extends HttpServlet {
 			usuarios = new HashMap<String, Usuario>();
 			app.setAttribute("usuarios", usuarios);
 		}
-		
+
 		boolean error = false;
 		// Intenta guardar un usuario. Si existe el identificador, devuelve un error
 		if (usuarios.get(u.getUsuario()) != null) {
-			//response.sendError(500, "Identificador de usuario duplicado");
-			//return;
+			// response.sendError(500, "Identificador de usuario duplicado");
+			// return;
 			error = true;
 		} else {
 			usuarios.put(u.getUsuario(), u);
 		}
 
-		// Omitimos el código de respuesta. Por defecto OK
-		// Establecemos el tipo MIME de la respuesta
 		response.setContentType("text/html");
-		// Escribimos la respuesta
 		PrintWriter out = response.getWriter();
-		// Esto siempre hay que hacerlo para un fichero HTML
-		out.println("<html>");
-		out.println("<head>");
-		// Establecemos el título de la página HTML
-		out.println("<title>" + "Procesamiento Datos Cliente" + "</title>");
-		out.println("</head>");
-		// Cuerpo de la página
-		out.println("<body>");
-		
-		// Obtiene la URL precedente
+
+		out.println(TOP);
+
 		String referer = request.getHeader("referer");
-		// Establece la cabecera de refresco
-		response.setHeader("refresh", "3; URL=" + referer);
-		
 		if (!error) {
-			// Nuestros datos
-			out.println("<b><p> Datos Cliente Procesados </p></b>");
+			out.println("Registro correcto");
+			response.setHeader("refresh", "3; URL=index.html");
 		} else {
-			out.println("<h1> Error: usuario duplicado </h1>");
+			out.println("Error: Usuario duplicado");
+			response.setHeader("refresh", "3; URL=" + referer);
 		}
 
-		// Cierre de la página
-		out.println("</body>");
-		out.println("</html>");
-		
+		out.println(BOTTOM);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
