dad7eb05aca93cfa30dac5516531e8631b48d01d
cr15th1an <cristhianraul.aramayor@um.es>
2018-12-16 16:24:51 +0100

    Se ha terminado de borrar datos innecesarios para la entrega

--- src/controller/Controlador.java
+++ src/controller/Controlador.java
@@ -23,7 +23,7 @@ public class Controlador {
 	private static Controlador unicaInstancia = null;
 	private Usuario usuarioLogeado = null;
 
-	public static final String FECHA_SISTEMA = "26/02/2019";
+	public static final String FECHA_SISTEMA = "26/02/2018";
 	public static Date FECHA_SISTEMA_DATE = null;
 
 	private Controlador() {
